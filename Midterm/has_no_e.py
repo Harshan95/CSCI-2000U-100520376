c06522b3a04c60a386e8fd7170c0214790f6af7a
Harshan <Harshan95@gmail.com>
2015-10-28 10:43:59 -0400

    commited by Armin

---
+++ Midterm/has_no_e.py
@@ -0,0 +1,13 @@
+def has_no_e(input)
+if(input='e')
+return false;
+else
+return true;
+
+
+reader=open('filename');
+int i=0;
+while reader.readline(i)!=" ":
+has_no_e(file.readline(i));
+i++;
+done;
---
+++ Midterm/strip.sh
@@ -0,0 +1,9 @@
+#!
+echo Enter number of lines to strip from the top of the file;
+$k=input;
+echo Enter number of lines to strip from the bottom of the file;
+$m=input;
+echo Enter the name of the file;
+$filename=input;
+head -filename -m+k
+# harshan 100520376
